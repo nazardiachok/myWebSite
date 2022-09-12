5d02dc09838dc2fb8fac0153fd2d8a621f9076ff
Nazar Diachok <diafcc55@gmail.com>
2022-09-12 20:53:53 +0200

    fehler im popup

--- myPage/script.js
+++ myPage/script.js
@@ -1,6 +1,32 @@
-const menuBurger = document.querySelector(".header__burger");
-const popup = document.querySelector(".header__burger");
+const burger = document.querySelector(".header__burger");
+const popup = document.querySelector(".popup");
+ 
+if (burger)
+/* if (popup) */
+burger.addEventListener('click', hamHandler);
+function hamHandler (e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
 
-menuBurger.addEventListener ('click', () => {
+    console.log("burger clicked")
+}
+
+
+/* 
+function hamHandler (e) {
+    if(popup){
+        e.preventDefault();
+    popup.classList.toggle("open")
+    }
+} */
+/* ================ */
+
+/* window.onload=function(){
     
-})
+    burger.addEventListener('click', hamHandler);
+    
+    function hamHandler (e) {
+        e.preventDefault();
+        popup.classList.toggle("open")
+    }
+} */
